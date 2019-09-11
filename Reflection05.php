b769ab03bc61f7245606fa424ae9da71d92d3db6
chifuyuu <52141554+chifuyuu@users.noreply.github.com>
2019-09-11 23:02:19 +0800

    Update Reflection05.php

--- Reflection05.php
+++ Reflection05.php
@@ -183,10 +183,6 @@
                                         </ul>
                                     </div><!-- end blog-meta -->
 
-                                    <div class="blog-media">
-                                        <img src="https://scontent.fcrk1-1.fna.fbcdn.net/v/t31.0-8/p960x960/15994835_885984614870492_6893341064443369026_o.jpg?_nc_cat=108&_nc_oc=AQlwQ_Uw71ilL02S7SBWqxfPT-Pqvdu4cXoVRXOn5tvQB1QZlykTTCKrhUQtXEo0Jlk&_nc_ht=scontent.fcrk1-1.fna&oh=d8b6c2bf6516ee9f5c34ac57c9e54119&oe=5DFFA447" alt="" class="img-responsive img-rounded">
-                                    </div><!-- end media -->
-
                                     <div class="blog-desc-big">
                                         <p class="lead">Fourth Week</p>
                                         <p>First Quiz.
