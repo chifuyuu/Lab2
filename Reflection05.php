afba68c245994c12bb3588edca910e8ada1dfdbd
chifuyuu <52141554+chifuyuu@users.noreply.github.com>
2019-09-11 22:55:32 +0800

    Update Reflection05.php

--- Reflection05.php
+++ Reflection05.php
@@ -188,27 +188,21 @@
                                     </div><!-- end media -->
 
                                     <div class="blog-desc-big">
-                                        <p class="lead">What I can say is things are great, they've never been better! </p>
-                                        <p>I am Paul Anthony C. Ragsac. I am now 19 years old as of now. I am from Las Piñas City.
+                                        <p class="lead">SoCiT Week</p>
+                                        <p>This year’s SoCit week was so educational.
 
 
 
-
-</p></br><strong>
-Seek stories.</strong></br></br>
-
-I first traveled by myself to Morong, Rizal because I had a special someone to meet and find it quite exciting travelling by myself because you really need to focus on where you are and not to pass over the exact place you want to get off. That day was kind of exhausting in my part but overall it was an experience that I will never forget.</br></br>
- 
-When I started studying in College it was an unexpected adventure for me since there are many things to learn and there are many challenges waiting ahead. The adventure is still on going and as I go along, there are many people I met some stayed and some already left the adventure. For me this adventure is just getting started and I am ready to face my next year in college. Hello 2nd Year!!!</br></br>
-
-“What would I find surprising about how you . . .
-First year was quite alarming for me since I don't have any background in programming and other computer related knowledge which some of my classmates already know. What I did was I studied them on my own and hoping that I will catch up and never left behind in every class I take. Now I am already in 2nd year and the fruit of my hard work paid but it was just getting started.</br></br>
- 
-My summer is a mess. I could not describe it in any fine way since I did not do any good this summer. Well I enjoyed my summer, but it was not a productive summer since I did not learn any new things. What I did? I just played ML and watch Netflix all summer :) 
+</p></br>
+<p>
+The speakers shared their own experiences on how they started from what they were before to now. The speakers were CEO’s of their own company which kind of make it for me interesting because you can really see how much effort they do in building it up. </br></br>I still hoped that the SOCIT fest next time would hold gaming tournament like DOTA 2 or LOL not only mobile legends and Tekken because it’s a downer because some people, like me does not play much console games. We could’ve joined the mobile legends tournament, but we had some things to do so in our other subjects so we couldn’t join anyways. Overall it was okay for some but for me it’s kind of not good, so I’m hoping for a better fest next year. 
+</br></br>
+<div class="image_image"><img src="https://i.imgur.com/35VcVaF.png" alt="" class="img-responsive img-rounded">
+</br></br>
+<div class="image_image"><img src="https://i.imgur.com/yJe7XrC.png" alt="" class="img-responsive img-rounded">
+</br></br>
+<div class="image_image"><img src="https://i.imgur.com/DuciOjK.png" alt="" class="img-responsive img-rounded">
 </p>
-
-                                        <p></br><strong>Talk about feelings.</br></br></strong> Dig deeper by following up.
-I feel so sleepy most of the time because I can't sleep early, and the class starts at 7:30 in the morning and I need to wake up again early my body can’t comprehend but it is what it is. Well in class I still try to be awake all the time, so I don't miss anything important</p>
 
                                         <hr class="invis">
 							<div class="content boxed-comment clearfix">
@@ -235,9 +229,11 @@ I feel so sleepy most of the time because I can't sleep early, and the class sta
                         <div class="sidebar col-md-4">
                             <div class="widget clearfix">
                                 <div class="banner-widget">
-								</br></br></br></br></br></br></br></br>
+					</br></br>
+								<div class="image_image_r">
                                     <img src="https://images.vexels.com/media/users/3/127525/isolated/preview/d79fa485e112044702de7470c8a48fc6-cute-meow-emoji-icon-by-vexels.png" alt="" class="img-responsive img-rounded"><h4><center>Lab2-Ragsac</center></h4>
                                 </div>
+								</div>
                             </div>
 
                             <div class="widget clearfix">
@@ -246,7 +242,7 @@ I feel so sleepy most of the time because I can't sleep early, and the class sta
                                     <div class="media">
                                         <img src="upload/blog_small_01.jpg" alt="" class="img-responsive alignleft img-rounded">
                                         <div class="media-body">
-                                            <h5 class="mt-0"><a href="blog-single.php">Reflection06</a></h5>
+                                            <h5 class="mt-0"><a href="blog-single.php">Reflection05</a></h5>
                                             <div class="blog-meta">
                                                 <ul class="list-inline">
                                                     <li><span>by</span> <a href="#">Yours Truly</a></li>
@@ -258,7 +254,7 @@ I feel so sleepy most of the time because I can't sleep early, and the class sta
                                     <div class="media">
                                         <img src="upload/blog_small_02.jpg" alt="" class="img-responsive alignleft img-rounded">
                                         <div class="media-body">
-                                            <h5 class="mt-0"><a href="blog-single.php">Reflection07</a></h5>
+                                            <h5 class="mt-0"><a href="blog-single.php">Reflection06</a></h5>
                                             <div class="blog-meta">
                                                 <ul class="list-inline">
                                                     <li>5 days ago</li>
@@ -271,7 +267,7 @@ I feel so sleepy most of the time because I can't sleep early, and the class sta
                                     <div class="media">
                                         <img src="upload/blog_small_03.jpg" alt="" class="img-responsive alignleft img-rounded">
                                         <div class="media-body">
-                                            <h5 class="mt-0"><a href="blog-single.php">Reflection08</a></h5>
+                                            <h5 class="mt-0"><a href="blog-single.php">Reflection07</a></h5>
                                             <div class="blog-meta">
                                                 <ul class="list-inline">
                                                     <li>6 days ago</li>
