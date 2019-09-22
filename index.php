f0f534f331eadc268244fadd40f98ff9094f160f
chifuyuu <52141554+chifuyuu@users.noreply.github.com>
2019-09-23 02:17:14 +0800

    Update index.php

--- index.php
+++ index.php
@@ -87,15 +87,17 @@
                                                 <div class="col-md-6 col-sm-6">
                                                     <h4>Activities</h4>
                                                     <ul>
-                                                        <li><a href="Activity01.php">Activity01</a></li>
-                                                        <li><a href="Activity02.php">Activity02</a></li>
-                                                        <li><a href="Activity03.php">Activity03</a></li>
-                                                        <li><a href="Activity04.php">Activity04</a></li>
-                                                        <li><a href="Activity05.php">Activity05</a></li>
-                                                        <li><a href="Activity06.php">Activity06</a></li>
-                                                        <li><a href="Activity07.php">Activity07</a></li>
-                                                        <li><a href="Activity08.php">Activity08</a></li>
-							</ul>
+                                                        <li><a href="Activity01.php">Activity01 - A1: Introduction & Accounts Creation</a></li>
+                                                        <li><a href="Activity02.php">Activity02 - A2: Tell Me About Your Favorite Application</a></li>
+                                                        <li><a href="Activity03.php">Activity03 - A3: How would you design an interface for a 1000 floor elevator?</a></li>
+                                                        <li><a href="Activity04.php">Activity04 - A4: Need Finding</a></li>
+                                                        <li><a href="Activity05.php">Activity05 - A5: Prototype - Registration and Login Forms</a></li>
+                                                        <li><a href="Activity06.php">Activity06 - A6: Persona</a></li>
+                                                        <li><a href="Activity07.php">Activity07 - A7 : Information Design</a></li>
+                                                        <li><a href="Activity08.php">Activity08 - A8: Grid layout Design</a></li>
+                                                        <li><a href="Activity09.php">Activity09 - A9: 12-Column Grid Design</a></li>
+														<li><a href="Activity10.php">Activity10</a></li>
+                                                    </ul>
                                                 </div>
                                         </div>
                                     </li>
@@ -109,13 +111,15 @@
                                                 <div class="col-md-6 col-sm-6">
                                                     <h4>Reflections</h4>
                                                     <ul>
-                                                        <li><a href="Reflection01.php">Reflection01</a></li>
-                                                        <li><a href="Reflection02.php">Reflection02</a></li>
-                                                        <li><a href="Reflection03.php">Reflection03</a></li>
-                                                        <li><a href="Reflection04.php">Reflection04</a></li>
-                                                        <li><a href="Reflection05.php">Reflection05</a></li>
-                                                        <li><a href="Reflection06.php">Reflection06</a></li>
-							 <li><a href="Quiz01.php">Quiz01</a></li>
+                                                        <li><a href="#">Reflection01</a></li>
+                                                        <li><a href="#">Reflection02</a></li>
+                                                        <li><a href="#">Reflection03</a></li>
+                                                        <li><a href="#">Reflection04</a></li>
+                                                        <li><a href="#">Reflection05</a></li>
+                                                        <li><a href="#">Reflection06</a></li>
+                                                        <li><a href="#">Reflection07</a></li>
+                                                        <li><a href="#">Reflection08</a></li>
+                                                        <li><a href="#">Reflection09</a></li>
                                                     </ul>
                                                 </div>
                                         </div>
@@ -131,10 +135,12 @@
                                                 <div class="col-md-6 col-sm-6">
                                                     <h4>Learnings</h4>
                                                     <ul>
-                                                        <li><a href="#">Learning01</a></li>
-                                                        <li><a href="#">Learning02</a></li>
-                                                        <li><a href="#">Learning03</a></li>
-                                                        <li><a href="#">Learning04</a></li>
+                                                        <li><a href="Learning01.php">Learning01</a></li>
+                                                        <li><a href="Learning02.php">Learning02</a></li>
+                                                        <li><a href="Learning03.php">Learning03</a></li>
+                                                        <li><a href="Learning04.php">Learning04</a></li>
+							<li><a href="Learning05.php">Learning05</a></li>
+                                                        <li><a href="Learning06.php">Learning06</a></li>
                                                     </ul>
                                                 </div>
                                         </div>
@@ -146,6 +152,7 @@
                 </nav><!-- end navbar -->
             </div><!-- end container -->
         </header>
+		
         <section id="home" class="video-section js-height-full">
             <div class="overlay"></div>
             <div class="home-text-wrapper relative container">
