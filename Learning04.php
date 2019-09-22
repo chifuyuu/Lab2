f1eadb406d73c2f119b5fb3188fa05c90c4665f5
chifuyuu <52141554+chifuyuu@users.noreply.github.com>
2019-09-23 02:28:44 +0800

    Add files via upload

---
+++ Learning04.php
@@ -0,0 +1,328 @@
+<!doctype html>
+<!--[if IE 9]> <html class="no-js ie9 fixed-layout" lang="en"> <![endif]-->
+<!--[if gt IE 9]><!--> <html class="no-js " lang="en"> <!--<![endif]-->
+<head>
+
+    <!-- Basic -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    
+    <!-- Mobile Meta -->
+    <meta name="viewport" content="width=device-width, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
+    
+    <!-- Site Meta -->
+    <title>Lab2-Ragsac</title>
+    
+    <!-- Site Icons -->
+
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,400i,500,700,900" rel="stylesheet"> 
+    <link href="https://fonts.googleapis.com/css?family=Droid+Serif:400,400i,700,700i" rel="stylesheet"> 
+    
+    <!-- Custom & Default Styles -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/font-awesome.min.css">
+    <link rel="stylesheet" href="css/carousel.css">
+    <link rel="stylesheet" href="css/animate.css">
+    <link rel="stylesheet" href="style.css">
+
+
+    <!--[if lt IE 9]>
+        <script src="js/vendor/html5shiv.min.js"></script>
+        <script src="js/vendor/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+<body>  
+
+    <!-- LOADER -->
+    <div id="preloader">
+        <img class="preloader" src="images/loader.gif" alt="">
+    </div><!-- end loader -->
+    <!-- END LOADER --> 
+    
+        <header class="header">
+            <div class="topbar clearfix">
+                <div class="container">
+                    <div class="row-fluid">
+                        <div class="col-md-6 col-sm-6 text-left">
+                            <p>
+                                <strong><i class="fa fa-phone"></i></strong> +98 765 432 1 &nbsp;&nbsp;
+                                <strong><i class="fa fa-envelope"></i></strong> <a href="mailto:#">paulragsac@gmail.com</a>
+                            </p>
+                        </div><!-- end left -->
+                        <div class="col-md-6 col-sm-6 hidden-xs text-right">
+                            <div class="social">
+                                <a class="facebook" href="https://www.facebook.com/Lolicon.Knight" data-tooltip="tooltip" data-placement="bottom" title="Facebook"><i class="fa fa-facebook"></i></a>              
+                                <a class="youtube" href="https://www.facebook.com/ChifuyuGaming" data-tooltip="tooltip" data-placement="bottom" title="youtube"><i class="fa fa-youtube"></i></a>
+                                <a class="linkedin" href="https://www.linkedin.com/in/paul-anthony-ragsac-1b735a15b/" data-tooltip="tooltip" data-placement="bottom" title="Linkedin"><i class="fa fa-linkedin"></i></a>
+                            </div><!-- end social -->
+                        </div><!-- end left -->
+                    </div><!-- end row -->
+                </div><!-- end container -->
+            </div><!-- end topbar -->
+
+            <div class="container">
+                <nav class="navbar navbar-default yamm">
+                    <div class="navbar-header">
+                        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                            <span class="sr-only">Toggle navigation</span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                        </button>
+                        <div class="logo-normal">
+                            <a class="navbar-brand" href="index.php"><img src="https://images.vexels.com/media/users/3/127525/isolated/preview/d79fa485e112044702de7470c8a48fc6-cute-meow-emoji-icon-by-vexels.png" alt=""><h4>Ragsac</h4></a>
+                        </div>
+                    </div>
+
+                    <div id="navbar" class="navbar-collapse collapse">
+                        <ul class="nav navbar-nav navbar-right">
+                            <li><a href="index.php">Home</a></li>
+                            <li class="dropdown yamm-fw yamm-half"><a href="#" data-toggle="dropdown" class="dropdown-toggle active">Activities <b class="fa fa-angle-down"></b></a>
+                                <ul class="dropdown-menu">
+                                    <li>
+                                        <div class="yamm-content clearfix">
+                                            <div class="row-fluid">
+                                                <div class="col-md-6 col-sm-6">
+                                                    <h4>Activities</h4>
+                                                    <ul>
+                                                        <li><a href="Activity01.php">Activity01 - A1: Introduction & Accounts Creation</a></li>
+                                                        <li><a href="Activity02.php">Activity02 - A2: Tell Me About Your Favorite Application</a></li>
+                                                        <li><a href="Activity03.php">Activity03 - A3: How would you design an interface for a 1000 floor elevator?</a></li>
+                                                        <li><a href="Activity04.php">Activity04 - A4: Need Finding</a></li>
+                                                        <li><a href="Activity05.php">Activity05 - A5: Prototype - Registration and Login Forms</a></li>
+                                                        <li><a href="Activity06.php">Activity06 - A6: Persona</a></li>
+                                                        <li><a href="Activity07.php">Activity07 - A7 : Information Design</a></li>
+                                                        <li><a href="Activity08.php">Activity08 - A8: Grid layout Design</a></li>
+                                                        <li><a href="Activity09.php">Activity09 - A9: 12-Column Grid Design</a></li>
+                                                        <li><a href="Activity10.php">Activity10</a></li>
+                                                    </ul>
+                                                </div>
+                                        </div>
+                                    </li>
+                                </ul>
+                            </li>
+                            <li class="dropdown yamm-fw yamm-half"><a href="#" data-toggle="dropdown" class="dropdown-toggle active">Reflections<b class="fa fa-angle-down"></b></a>
+                                <ul class="dropdown-menu">
+                                    <li>
+                                        <div class="yamm-content clearfix">
+                                            <div class="row-fluid">
+                                                <div class="col-md-6 col-sm-6">
+                                                    <h4>Reflections</h4>
+                                                    <ul>
+                                                        <li><a href="#">Reflection01</a></li>
+                                                        <li><a href="#">Reflection02</a></li>
+                                                        <li><a href="#">Reflection03</a></li>
+                                                        <li><a href="#">Reflection04</a></li>
+                                                        <li><a href="#">Reflection05</a></li>
+                                                        <li><a href="#">Reflection06</a></li>
+                                                        <li><a href="#">Reflection07</a></li>
+                                                        <li><a href="#">Reflection08</a></li>
+                                                        <li><a href="#">Reflection09</a></li>
+                                                    </ul>
+                                                </div>
+                                        </div>
+                                    </li>
+                                </ul>
+                            </li>
+                            
+                            <li class="dropdown yamm-fw yamm-half"><a href="#" data-toggle="dropdown" class="dropdown-toggle active">Learnings<b class="fa fa-angle-down"></b></a>
+                                <ul class="dropdown-menu">
+                                    <li>
+                                        <div class="yamm-content clearfix">
+                                            <div class="row-fluid">
+                                                <div class="col-md-6 col-sm-6">
+                                                    <h4>Learnings</h4>
+                                                    <ul>
+                                                        <li><a href="Learning01.php">Learning01</a></li>
+                                                        <li><a href="Learning02.php">Learning02</a></li>
+                                                        <li><a href="Learning03.php">Learning03</a></li>
+                                                        <li><a href="Learning04.php">Learning04</a></li>
+                            <li><a href="Learning05.php">Learning05</a></li>
+                                                        <li><a href="Learning06.php">Learning06</a></li>
+                                                    </ul>
+                                                </div>
+                                        </div>
+                                    </li>
+                        
+                        </ul>
+                    <li><a href="page-contact.php">Contact</a></li>
+                    </div>
+                </nav><!-- end navbar -->
+            </div><!-- end container -->
+        </header>
+
+        <section class="section db p120">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="tagline-message page-title text-center">
+                            <h3></br></br>Activity08</h3>
+                            <ul class="breadcrumb">
+                                <li><a href="javascript:void(0)">Ragsac</a></li>
+                                <li class="active">Activity</li>
+                            </ul>
+                        </div>
+                    </div><!-- end col -->
+                </div><!-- end row -->
+            </div><!-- end container -->
+        </section><!-- end section -->
+
+        <section class="section gb nopadtop">
+            <div class="container">
+                <div class="boxed">
+                    <div class="row">
+                        <div class="col-md-8">
+                            <div class="content blog-list">
+                                <div class="blog-wrapper clearfix">
+                                    <div class="blog-meta">
+                                        <small><a href="#">Tips & Tricks</a></small>
+                                        <h3>Activity08</h3>
+                                        <ul class="list-inline">
+                                            <li>6 hours ago</li>
+                                            <li><span>written by</span> <a href="#">Paul Anthony Ragsac</a></li>
+                                        </ul>
+                                    </div><!-- end blog-meta -->
+
+                                    <div class="blog-desc-big">
+                                        <p class="lead">LinkedIn Learning Certificate 4</p></br></br>
+
+<p>
+
+<div class="image_image"><img src="https://i.imgur.com/HVhuIbO.png" alt="" class="img-responsive img-rounded"></div><!-- end media --></br></br>
+
+</p>
+
+                                        <hr class="invis">
+                            <div class="content boxed-comment clearfix">
+                                <div class="comments-list">
+                                        <div class="tags-widget">   
+                                            <ul class="list-inline">
+                                                <li><a href="#">course</a></li>
+                                                <li><a href="#">web design</a></li>
+                                                <li><a href="#">development</a></li>
+                                                <li><a href="#">language</a></li>
+                                            </ul>
+                                        </div><!-- end list-widget -->
+
+                                    </div><!-- end desc -->
+                                </div><!-- end blog -->
+                            </div><!-- end content -->
+                            
+                                    <div class="media">
+                                    </div>
+                                </div>
+                            </div><!-- end content -->
+                        </div><!-- end col -->
+
+                        <div class="sidebar col-md-4">
+                            <div class="widget clearfix">
+                                <div class="banner-widget">
+                    </br></br>
+                                <div class="image_image_r">
+                                    <img src="https://images.vexels.com/media/users/3/127525/isolated/preview/d79fa485e112044702de7470c8a48fc6-cute-meow-emoji-icon-by-vexels.png" alt="" class="img-responsive img-rounded"><h4><center>Lab2-Ragsac</center></h4>
+                                </div>
+                                </div>
+                            </div>
+                        </div><!-- end sidebar -->
+                    </div><!-- end row -->
+                </div><!-- end boxed -->
+            </div><!-- end container -->
+        </section>
+
+         <footer class="section footer noover">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-4 col-md-4">
+                        <div class="widget clearfix">
+                            <h3 class="widget-title">Subscribe Our Newsletter</h3>
+                            <div class="newsletter-widget">
+                                <p>You can opt out of our newsletters at any time.<br> See our <a href="#">privacy policy</a>.</p>
+                                <form class="form-inline" role="search">
+                                    <div class="form-1">
+                                        <input type="text" class="form-control" placeholder="Enter email here..">
+                                        <button type="submit" class="btn btn-primary"><i class="fa fa-paper-plane-o"></i></button>
+                                    </div>
+                                </form>
+                            </div><!-- end newsletter -->
+                        </div><!-- end widget -->
+                    </div><!-- end col -->
+
+                    <div class="col-lg-3 col-md-3">
+                        <div class="widget clearfix">
+                            <h3 class="widget-title">Learn More Today</h3>
+                            <p>Would you like to learn more? Click this link</p>
+                            <a href="https://www.facebook.com/ChifuyuGaming" class="readmore">Become a follower</a>
+                        </div><!-- end widget -->
+                    </div><!-- end col -->
+
+                    <div class="col-lg-3 col-md-3">
+                        <div class="widget clearfix">
+                            <h3 class="widget-title">Popular Tags</h3>
+                            <div class="tags-widget">   
+                                <ul class="list-inline">
+                                    <li><a href="#">course</a></li>
+                                    <li><a href="#">web design</a></li>
+                                    <li><a href="#">development</a></li>
+                                    <li><a href="#">language</a></li>
+                                    <li><a href="#">teacher</a></li>
+                                    <li><a href="#">speaking</a></li>
+                                    <li><a href="#">material</a></li>
+                                    <li><a href="#">css3</a></li>
+                                    <li><a href="#">html</a></li>
+                                    <li><a href="#">learning</a></li>
+                                </ul>
+                            </div><!-- end list-widget -->
+                        </div><!-- end widget -->
+                    </div><!-- end col -->
+
+                    <div class="col-lg-2 col-md-2">
+                        <div class="widget clearfix">
+                            <h3 class="widget-title">Support</h3>
+                            <div class="list-widget">   
+                                <ul>
+                                    <li><a href="#">Service</a></li>
+                                    <li><a href="#">Smile</a></li>
+                                    <li><a href="#">Sweet</a></li>
+                                    <li><a href="#">Sister</a></li>
+                                    <li><a href="#">Sadistic</a></li>
+                                    <li><a href="#">Surprise</a></li>
+                                </ul>
+                            </div><!-- end list-widget -->
+                        </div><!-- end widget -->
+                    </div><!-- end col -->
+                </div><!-- end row -->
+            </div><!-- end container -->
+        </footer><!-- end footer -->
+
+        <div class="copyrights">
+            <div class="container">
+                <div class="clearfix">
+                    <div class="pull-left">
+                         <div class="logo-normal">
+                            <a class="navbar-brand" href="index.php"><img src="https://images.vexels.com/media/users/3/127525/isolated/preview/d79fa485e112044702de7470c8a48fc6-cute-meow-emoji-icon-by-vexels.png" alt=""></a>
+                        </div>
+                    </div>
+
+                    <div class="pull-right">
+                        <div class="footer-links">
+                            <ul class="list-inline">
+                                <li>Design : <a href="https://html.design">HTML.Design</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div><!-- end container -->
+        </div><!-- end copy -->
+    </div><!-- end wrapper -->
+
+    <!-- jQuery Files -->
+    <script src="js/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/carousel.js"></script>
+    <script src="js/animate.js"></script>
+    <script src="js/custom.js"></script>
+    <script src="js/videobg.js"></script>
+</body>
+</html>
