7c44b3efd708ac2f738be0a1cc81012f9cfea504
chifuyuu <52141554+chifuyuu@users.noreply.github.com>
2019-09-23 09:06:37 +0800

    Update Activity01.php

--- Activity01.php
+++ Activity01.php
@@ -84,19 +84,19 @@
                                     <li>
                                         <div class="yamm-content clearfix">
                                             <div class="row-fluid">
-                                                <div class="col-md-6 col-sm-6">
+                                                <div class="col-md-10 col-sm-10">
                                                     <h4>Activities</h4>
                                                     <ul>
                                                         <li><a href="Activity01.php"><small>Activity01 - A1: Introduction & Accounts Creation</small></a></li>
-                                                        <li><a href="Activity02.php"></small>Activity02 - A2: Tell Me About Your Favorite Application</small></a></li>
-                                                        <li><a href="Activity03.php"></small>Activity03 - A3: How would you design an interface for a 1000 floor elevator?</small></a></li>
-                                                        <li><a href="Activity04.php"></small>Activity04 - A4: Need Finding</small></a></li>
-                                                        <li><a href="Activity05.php"></small>Activity05 - A5: Prototype - Registration and Login Forms</small></a></li>
-                                                        <li><a href="Activity06.php"></small>Activity06 - A6: Persona</small></a></li>
-                                                        <li><a href="Activity07.php"></small>Activity07 - A7 : Information Design</small></a></li>
-                                                        <li><a href="Activity08.php"></small>Activity08 - A8: Grid layout Design</small></a></li>
-                                                        <li><a href="Activity09.php"></small>Activity09 - A9: 12-Column Grid Design</small></a></li>
-														<li><a href="Activity10.php"></small>Activity10</small></a></li>
+                                                        <li><a href="Activity02.php"><small>Activity02 - A2: Tell Me About Your Favorite Application</small></a></li>
+                                                        <li><a href="Activity03.php"><small>Activity03 - A3: How would you design an interface for a 1000 floor elevator?</small></a></li>
+                                                        <li><a href="Activity04.php"><small>Activity04 - A4: Need Finding</small></a></li>
+                                                        <li><a href="Activity05.php"><small>Activity05 - A5: Prototype - Registration and Login Forms</small></a></li>
+                                                        <li><a href="Activity06.php"><small>Activity06 - A6: Persona</small></a></li>
+                                                        <li><a href="Activity07.php"><small>Activity07 - A7 : Information Design</small></a></li>
+                                                        <li><a href="Activity08.php"><small>Activity08 - A8: Grid layout Design</small></a></li>
+                                                        <li><a href="Activity09.php"><small>Activity09 - A9: 12-Column Grid Design</small></a></li>
+														<li><a href="Activity10.php"><small>Activity10</small></a></li>
                                                     
 							    </ul>
                                                 </div>
