9b5ba23ee7c388264812883c586f0226740e0ccd
chifuyuu <52141554+chifuyuu@users.noreply.github.com>
2019-09-25 06:25:01 +0800

    Update Learning04.php

--- Learning04.php
+++ Learning04.php
@@ -112,15 +112,15 @@
                                                 <div class="col-md-6 col-sm-6">
                                                     <h4>Reflections</h4>
                                                     <ul>
-                                                        <li><a href="#">Reflection01</a></li>
-                                                        <li><a href="#">Reflection02</a></li>
-                                                        <li><a href="#">Reflection03</a></li>
-                                                        <li><a href="#">Reflection04</a></li>
-                                                        <li><a href="#">Reflection05</a></li>
-                                                        <li><a href="#">Reflection06</a></li>
-                                                        <li><a href="#">Reflection07</a></li>
-                                                        <li><a href="#">Reflection08</a></li>
-                                                        <li><a href="#">Reflection09</a></li>
+                                                        <li><a href="Reflection01.php">Reflection01</a></li>
+                                                        <li><a href="Reflection02.php">Reflection02</a></li>
+                                                        <li><a href="Reflection03.php">Reflection03</a></li>
+                                                        <li><a href="Reflection04.php">Reflection04</a></li>
+                                                        <li><a href="Reflection05.php">Reflection05</a></li>
+                                                        <li><a href="Reflection06.php">Reflection06</a></li>
+                                                        <li><a href="Reflection07.php">Reflection07</a></li>
+                                                        <li><a href="Reflection08.php">Reflection08</a></li>
+                                                        <li><a href="Reflection09.php">Reflection09</a></li>
                                                     </ul>
                                                 </div>
                                         </div>
